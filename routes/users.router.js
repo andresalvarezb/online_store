75ea762892cad144109c0cd9c461bb68cabfccd6
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-04-05 06:57:11 -0500

    feat: creating users routing

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,22 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+const UsersService = require('../services/users.services');
+const service = new UsersService()
+
 router.get('/', (req, res) => {
-    const { limit, offset } = req.query;
-    if (limit && offset) {
-        res.json({
-            limit,
-            offset
-        })
-    } else {
-        res.send('there aren\'t params');
-    }
+	const users = service.find()
+    res.json(users)
 });
 
-router.get('/:userId', (req, res) => {
-    const { userId } = req.params;
-    res.send('I am user ', userId);
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+	const user = service.findOne(id)
+    res.json(user)
 });
 
+router.post('/', (req, res) => {
+	const body = req.body;
+	const newUser = service.create(body)
+	res.status(201).json(newUser)
+})
+
+router.patch('/:id', (req, res, next) => {
+	try {
+		const { id } = req.params
+		const body = req.body
+		const userUpdate = service.update(id, body)
+		res.json(userUpdate)
+	} catch (error) {
+		next(error)
+	}
+})
+
+router.delete('/:id', (req, res) => {
+	const {id} = req.params
+	const user = service.delete(id)
+	res.json({
+		user,
+		message: 'Delete user'
+	})
+})
 
 module.exports = router;
