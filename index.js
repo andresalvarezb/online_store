ef248fb229148a971e93bb97da2df2f293858bea
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-04-06 06:44:47 -0500

    feat: add cors logic

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 // import express
 const express = require('express');
 const routersApi = require('./routes');
+const cors = require('cors')
 
-const {logErrors, errorHandler, boomErrorHandler} = require('./middleware/error.handler');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handler');
 
 const app = express();
 // create port
 const port = 8080;
 
+const whiteList = ['http://localhost:8080', 'https://myapp.com']
+const options = {
+	origin: (origin, callback) => {
+		if (whiteList.includes(origin)) {
+			callback(null, true)
+		} else {
+			callback(new Error('don\'t allow'))
+		}
+	}
+}
 app.use(express.json());
+app.use(cors(options))
+
 routersApi(app);
 
 app.use(logErrors);
