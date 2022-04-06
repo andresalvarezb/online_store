ae93389b54a766b576f62ba86ca5dee4347b660e
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-04-06 08:36:14 -0500

    feta: create users' schemas

---
+++ schemas/users.schema.js
@@ -0,0 +1,28 @@
+const Joi = require('joi')
+
+const id = Joi.string().uuid();
+const name = Joi.string().min(3)
+const gender = Joi.string()
+const email = Joi.string.email()
+
+const createUsersSchema = Joi.object({
+	name: name.required(),
+	gender: gender.required(),
+	email: email.required(),
+})
+
+const getUsersSchema = Joi.object({
+	id: id.required()
+})
+
+const updateUsersSchema = Joi.object({
+	name: name,
+	gender: gender,
+	email: email,
+})
+
+module.exports = {
+	createUsersSchema,
+	getUsersSchema,
+	updateUsersSchema,
+}
